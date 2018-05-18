17b115a5a188a1907495545e0092e4682a6cd871
Linggarunkal <linggar@outlook.com>
2018-05-18 14:01:41 +0700

    modify all front end

--- projectManagement/controllers/route.py
+++ projectManagement/controllers/route.py
@@ -10,6 +10,7 @@ from flask import render_template
 def start():
     return render_template('auth/login.html')
 
+
 #login route logic
 @app.route("/login")
 def login():
@@ -21,15 +22,18 @@ def login():
 def dashboard():
     return render_template('content/home.html')
 
+
 #registration route logic
 @app.route("/user/registration")
 def registration():
     return render_template('auth/register.html')
 
+
 #forgot password route logic
 @app.route('/user/forgot')
 def forgotPasswd():
     return render_template('auth/forgot-password.html')
+
 
 #forgot password route logic
 @app.route('/user/changepassword')
@@ -42,60 +46,72 @@ def chgPasswd():
 def emplAll():
     return render_template('content/employee.html')
 
+
 #page edit detail employee
 @app.route('/employees/edit/detail')
 def empEdtDet():
     return render_template('content/edit-employee.html')
+
 
 #page department employee
 @app.route('/department/all')
 def deptAll():
     return render_template('content/department.html')
 
+
 #page edit detail department
 @app.route('/department/edit/detail')
 def deptEditDet():
     return render_template('content/edit-department.html')
+
 
 #page division
 @app.route('/division/all')
 def divAll():
     return render_template('content/division.html')
 
+
 #page edit detail division
 @app.route('/division/edit/detail')
 def divEditDet():
     return render_template('content/edit-division.html')
+
 
 #page main client
 @app.route("/user/main/client")
 def userMainHome():
     return render_template('content/main-client.html')
 
+
 #page edit client
 @app.route("/user/edit/client")
 def userEditClient():
     return render_template('content/edit-client.html')
+
 
 #page main client
 @app.route("/user/main/clientlist")
 def userMainHomeList():
     return render_template('content/main-clientlist.html')
 
+
 #page detail client
 @app.route("/user/client/detail")
 def userClientDetail():
     return render_template('content/detail-client.html')
+
 
 #page edit client
 @app.route("/user/client/edit")
 def userClientEdit():
     return render_template('content/edit-profile.html')
 
+
 #page main project
 @app.route("/project/main")
 def mainProject():
     return render_template('content/main-project.html')
+
 
 #page edit project
 @app.route("/project/edit/detail")
@@ -108,10 +124,12 @@ def editProject():
 def viewDetailProject():
     return render_template('content/detail-projectview.html')
 
+
 #page approve project
 @app.route("/project/approve")
 def projectApprove():
     return render_template('content/approve-project.html')
+
 
 #page document project
 @app.route("/project/document")
@@ -120,34 +138,64 @@ def projectDoc():
 
 
 #page task list
-@app.route("/project/tasklist")
+@app.route("/project/task/all")
 def projectTaskList():
     return render_template('content/list-task.html')
+
+
+#page edit task
+@app.route("/project/task/edit")
+def projectTaskEdit():
+    return render_template('content/edit-task.html')
+
+
+#page detail task
+@app.route("/project/task/detail")
+def projectTaskDetail():
+    return render_template('content/detail-task.html')
+
 
 #page time sheet
 @app.route("/project/timesheet")
 def projectTimesheet():
     return render_template('content/time-sheet.html')
 
-#page problem
+
+#page problem all
 @app.route("/problem/all")
 def problemAll():
     return render_template('content/problem-log.html')
+
+
+#page problem detail
+@app.route("/problem/detail")
+def problemDetail():
+    return render_template('content/detail-problem.html')
+
+
+#page problem edit
+@app.route("/problem/edit")
+def problemEdit():
+    return render_template('content/edit-problem.html')
+
 
 #page report proyek
 @app.route("/project/report/all")
 def reportProject():
     return render_template('content/report-proyek.html')
 
+
 #page report task
 @app.route("/project/report/task")
 def reportTask():
     return render_template('content/report-task.html')
 
+
 #page report problem
 @app.route("/project/report/problem")
 def reportProblem():
     return render_template('content/report-problem.html')
+
 
 #page report audit trail
 @app.route("/project/report/audit")
