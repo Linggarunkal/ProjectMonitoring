54df9c06d0441ae66bcf4a3ae1f4b875896d7e82
lkurniawan <linggar@outlook.com>
2018-07-03 18:11:12 +0700

    send mail class added

--- projectManagement/library/config.py
+++ projectManagement/library/config.py
@@ -3,3 +3,9 @@ HOST = '128.199.129.191'
 USERNAME = 'smulfa'
 PASSWORD = 'ArestoMomento_17'
 DATABASE = 'project_management'
+
+# email Setting
+SMTP_SERVER = "smtp.gmail.com: 587"
+MESSAGE_SUBJECT = "Project Management Notification"
+MESSAGE_FROM = "14linggar@gmail.com"
+SMTP_PASSWORD = "insya allah aku sukses"
